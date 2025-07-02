c519094a7d5a9803f649b0475f2305cd396f5467
C0D3Dr4G0N <brennanj18@southernct.edu>
2025-07-02 11:35:22 -0400

    added dynamic forms for editing personal information

--- views/profile.php
+++ views/profile.php
@@ -32,14 +32,6 @@
           <button class="edit-button btn" id="button-phoneNum" type="button">Edit</button>
         </fieldset>
         <fieldset class="field">
-          <label for="emailbox">Email Address:</label>
-          <input class="input" type="text" id="emailbox" value="<?php echo $_SESSION['email']; ?>" disabled="disabled" />
-        </fieldset>
-        <fieldset class="field">
-          <label for="studentID">Student ID:</label>
-          <input class="input" type="text" id="studentID" value="<?php echo $_SESSION['studentID']; ?>" disabled="disabled" />
-        </fieldset>
-        <fieldset class="field">
           <a href="reset_pass.php">Reset Password</a>  
         </fieldset>             
       </form>
@@ -47,9 +39,10 @@
   </main>
     <!-- Footer -->
   <?php include ROOT_PATH . "inc/footer.inc"; ?>
-  <script>
 
-    const editbutton = document.getElementsByClassName("edit-button");
+  // Dynamic form for editing information:
+  <script>
+    const editbutton = document.querySelectorAll('button');
 
     const newForm = document.createElement("form");
     newForm.setAttribute('action', '');
@@ -57,37 +50,41 @@
     newForm.setAttribute('class', 'form');
     newForm.style.marginTop = '5%';
 
-    const newInput = document.createElement('input');
-    newInput.setAttribute('type', 'text');
-    
-    for (let z = 0; z < editbutton.length; z++) {
-      if (editbutton[z].id === 'button-fname') {
-        newInput.setAttribute('name', 'fname');
-        newInput.setAttribute('placeholder', 'Enter your new first name');
-      } else if (editbutton[z].id === 'button-lname') {
-        newInput.setAttribute('name', 'lname');
-        newInput.setAttribute('placeholder', 'Enter your new last name');
-      } else if (editbutton[z].id === 'button-phoneNum') {
-        newInput.setAttribute('name', 'phoneNum');
-        newInput.setAttribute('placeholder', 'Enter your new phone number');
-      } else {
-        newInput.setAttribute = ('name', 'name');
-      }
-    };
-    
     const newSubmitButton = document.createElement('button');
     newSubmitButton.setAttribute('type', 'submit');
     newSubmitButton.setAttribute('class', 'btn');
     newSubmitButton.setAttribute('style', 'background-color: rgb(255, 204, 64)');
     newSubmitButton.textContent = 'Submit';
 
-    newForm.appendChild(newInput);
-    newForm.appendChild(newSubmitButton);
+    const newInput = document.createElement('input');
+    newInput.setAttribute('type', 'text');
 
-    for (let i = 0; i < editbutton.length; i++) {
-      editbutton[i].addEventListener('click', function() {
-        this.after(newForm);
+    document.addEventListener('DOMContentLoaded', function () {
+      editbutton.forEach(node => node.onclick = function (event) {
+        const nodeId = event.target.id;
+
+        if (nodeId === 'button-fname') {
+          newInput.setAttribute('name', 'fname');
+          newInput.setAttribute('placeholder', 'Enter your new first name');
+          newForm.appendChild(newInput);
+          newForm.appendChild(newSubmitButton);
+          this.after(newForm);
+        } else if (nodeId === 'button-lname') {          
+          newInput.setAttribute('name', 'lname');
+          newInput.setAttribute('placeholder', 'Enter your new last name');
+          newForm.appendChild(newInput);
+          newForm.appendChild(newSubmitButton);
+          this.after(newForm);
+        } else if (nodeId === 'button-phoneNum') {
+          newInput.setAttribute('name', 'phoneNum');
+          newInput.setAttribute('placeholder', 'Enter your new phone number');
+          newForm.appendChild(newInput);
+          newForm.appendChild(newSubmitButton);
+          this.after(newForm);
+        } else {
+          // do nothing.
+        }
       });
-    };
+    });
   </script>
 </body>
